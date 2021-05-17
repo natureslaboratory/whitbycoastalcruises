7ed72a6a623eec330a38eb94d0dfd5ea8818e4e2
Jack Barber <jack@natureslaboratory.co.uk>
2021-05-17 16:23:58 +0100

    Update header.php

--- perch/templates/layouts/header.php
+++ perch/templates/layouts/header.php
@@ -6,17 +6,25 @@
 	<title><?php perch_pages_title(); ?></title>
 	<?php perch_page_attributes(); ?>
 	<link rel="preconnect" href="https://fonts.gstatic.com">
-	<link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"> 
 	<link href="/assets/css/stylesheet.css?v=<?php echo rand(); ?>" rel="stylesheet" />
 	<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
 	<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+	<script src="https://js.stripe.com/v3/"></script>
 </head>
 <body>
 	<header class="background-primary">
 		<div class="restrict">
+			<div>
+				<a class="button" href="/book-online/">Book Online</a>
+			</div>
 			<nav>
 				<a class="menu button">&#8801; Menu</a>
 				<?php perch_pages_navigation(); ?>
 			</nav>
+			<div>
+				<p><a href="/shop/cart" class="cart"><?php perch_shop_cart_total(); ?></a>
+				<?php if(perch_member_logged_in()){echo '<a class="cart" href="/account">Account</a> <a class="cart" href="/account/logout">Logout</a>';}?></p>
+			</div>
 		</div>
 	</header>
