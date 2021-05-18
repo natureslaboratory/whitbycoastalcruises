510c5e4334eefafca5ced277fed26322217fb48d
Jack Barber <jack@natureslaboratory.co.uk>
2021-05-18 17:34:15 +0100

    Update book.php

--- perch/templates/pages/book.php
+++ perch/templates/pages/book.php
@@ -1,6 +1,12 @@
 <?php if (!defined('PERCH_RUNWAY')) include($_SERVER['DOCUMENT_ROOT'].'/perch/runtime.php'); ?>
-<?php	
-perch_shop_product(perch_get('id'), [
-    'template' => 'products/shop_product_book.html'
-]);
+<?php perch_layout('header'); ?>
+<?php perch_content('Page'); ?>
+<div class="text">
+    <div class="restrict">
+<?php
+	  st_event(perch_get('id'));
 ?>
+	</div>
+</div>
+<?php perch_content('Page Lower'); ?>
+<?php perch_layout('footer'); ?>
