f0065f65236f6f9cdbb981a3bcd297d661b83c93
Jack Barber <jack@natureslaboratory.co.uk>
2021-05-18 17:29:38 +0100

    Layout

--- perch/templates/pages/book_online.php
+++ perch/templates/pages/book_online.php
@@ -1,7 +1,7 @@
 <?php if (!defined('PERCH_RUNWAY')) include($_SERVER['DOCUMENT_ROOT'].'/perch/runtime.php'); ?>
 <?php perch_layout('header'); ?>
 <?php perch_content('Page'); ?>
-<div class="text">
+<div class="book">
     <div class="restrict"><?php st_events(); ?></div>
 </div>
 <?php perch_content('Page Lower'); ?>
